7b1337cc6a5f072a0513f440ebcaba252b050b6d
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-02-02 12:08:48 +0100

    unit clean up

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ def analyze_trace(bundle, group_id, series_id, trace_name, axs_start_idx, axs, f
 
         # ---- 1-exp fit ----
         if not np.isnan(A_fit):
-            fit1_vals = (fit_plot_y / F_to_pF).tolist()
+            fit1_vals = fit_plot_y.tolist()
             fit1_points = list(zip(fit_times, fit1_vals))
         else:
             fit1_points = []
