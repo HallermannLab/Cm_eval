8cecadca136345b08dd43e6c1b88d5edb0c61379
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-02-03 14:31:14 +0100

    update

--- main.py
+++ main.py
@@ -425,6 +425,7 @@ def analyze_trace(bundle, group_id, series_id, trace_name, axs_start_idx, axs, f
 
 def analyze_aps_trace(cm_trace, v_trace, time, axs, axs_start_idx, trace_name, file_name, group_id, series_id):
 
+def analyze_aps_trace(bundle, group_id, series_id, trace_name, axs_start_idx, axs, file_name):
     """
     aps analysis: baseline subtraction, median filter, fits.
     Uses same conventions as analyze_trace but WITHOUT voltage-based timing
