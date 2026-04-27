a328b6ed33fb2b2a955572be642770e56a96cd5b
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-04-27 14:54:38 +0200

    clean up figures

--- main.py
+++ main.py
@@ -543,8 +543,7 @@ def analyze_aps_trace(bundle, group_id, series_id, trace_name, axs_start_idx, ax
             ax(1).plot(time_rel, cm_bs, label="Baseline-subtracted")
 
             if not np.isnan(A1):
-                ax(1).plot(fit_plot_x, fit1, 'r--', label="Exponential fit")
-
+                ax(1).plot(fit_plot_x, fit1, 'r--', label=f"1-exp\nA={A1:.2f} pF, τ={tau1:.2f} s")
             ax(1).set_title("Baseline-subtracted + 1-exp fit")
             ax(1).legend()
             ax(1).set_xlabel("Time (s)")
