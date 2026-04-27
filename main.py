6adceabfe821b48f658e93b16c9e6bad27fe6bb5
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-04-27 15:32:34 +0200

    clean up

--- main.py
+++ main.py
@@ -1456,9 +1456,6 @@ def Cm_eval():
 
             print(f"Analyzing {trace_name}")
 
-            apsl_series_id = int(float(row[apsl_col])) - 1
-            trace_id = 2  # current trace
-
             aps_result = analyze_aps_trace(
                 bundle, group_id, aps_series_id,
                 trace_name, 0, None, file_name  # Pass None for axs for now
