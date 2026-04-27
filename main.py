0891478ca0d98bd1d65910c2d23de54171054584
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-04-27 14:48:05 +0200

    clean up

--- main.py
+++ main.py
@@ -257,7 +257,7 @@ def analyze_trace(bundle, group_id, series_id, trace_name, axs_start_idx, axs, f
             axs[axs_start_idx + 2].plot(fit_plot_x, fit_plot_y_expY, 'g--',
                                         label=f"1-expY fit\nA={A_fit_y:.2f} pF, τ={tau_fit_y:.2f} s, y0={y0_fit_y:.2f}")
         axs[axs_start_idx + 2].set_title("Baseline-subtracted + 1-expY fit")
-        axs[axs_start_idx + 2].legend()
+        axs[axs_start_idx + 2].legend(fontsize=8)
         axs[axs_start_idx + 2].set_xlabel("Time (s)")
         axs[axs_start_idx + 2].set_ylabel("pF")
 
