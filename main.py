1a64fa999d98e101773674166e9a29673a1a110d
synapsophie <sophie.schwitzgoebel@gmail.com>
2026-04-27 14:51:18 +0200

    clean up

--- main.py
+++ main.py
@@ -379,6 +379,8 @@ def analyze_trace(bundle, group_id, series_id, trace_name, axs_start_idx, axs, f
                                             ha='center', va='center', transform=axs[axs_start_idx + 4].transAxes)
                 axs[axs_start_idx + 4].set_title("Calcium current – No Data")
         else:
+            ca_current_trace = None
+            ca_current_time  = None
             axs[axs_start_idx + 4].text(0.5, 0.5, "t0 or t1 not found",
                                         ha='center', va='center', transform=axs[axs_start_idx + 4].transAxes)
             axs[axs_start_idx + 4].set_title("Current Trace - No Timing")
